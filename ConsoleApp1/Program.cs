a2f7969d3c1b986c6710360fcc5194ac8fd6d28a
StarGrim33 <prosky33@gmail.com>
2023-06-30 15:43:00 +0300

    Выполнил задачу

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        class Weapon
+        {
+            private int _bullets;
+
+            public bool CanShoot() => _bullets > Constants.EmptyMag;
+
+            public void Shoot() => _bullets -= Constants.BulletsPerShot;
+        }
+
+        class Constants
+        {
+            public static readonly int EmptyMag = 0;
+            public static readonly int BulletsPerShot = 1;
+        }
+    }
+}
